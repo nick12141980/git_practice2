3a9629052b5c83f392f5e3ab91771c2aa45509f9
Nick <nikolayshilov@yahoo.com>
2022-01-11 11:48:21 -0500

    new test case created

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -1,4 +1,10 @@
 package us100;
 
 public class TestCase002 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+        }
+
+    }
 }
